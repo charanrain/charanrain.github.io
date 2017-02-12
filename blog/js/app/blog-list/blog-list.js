21ab9b9c19ef70c282d8a044cb27cf612e899470
charanrain <cvcharanrj@gmail.com>
2017-02-12 18:01:30 +0530

    'update'

--- blog/js/app/blog-list/blog-list.js
+++ blog/js/app/blog-list/blog-list.js
@@ -4,11 +4,11 @@ var blog_list = angular.module('blog-list', []);
 blog_list.component('blogList', {
   templateUrl:'js/templates/blog-list.html',
   controller: function($http,$routeParams, $scope){
-    $http.get('core/posts/blog-data.json').then(successData,errorData);
-function successData(response, config, status, statusText){
+    $http.get('core/posts/blog-data.json').then(successCallback,errorCallback);
+function successCallback(response, config, status, statusText){
   var lists = response.data;
 }
-function errorData(response, config, status, statusText){
+function errorCallback(response, config, status, statusText){
 
 }
   }
