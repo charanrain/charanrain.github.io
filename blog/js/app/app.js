e99fa0ac9c3333eb6233fdd635b43cdad2562c37
charanrain <cvcharanrj@gmail.com>
2017-02-13 08:07:42 +0530

    update

--- blog/js/app/app.js
+++ blog/js/app/app.js
@@ -16,3 +16,19 @@ app.config(function($routeProvider){
     template:'<h1>Not Found</h1>'
   })
 });
+app.controller('postMe', function($http,$scope){
+  function dummy(ine){
+    var parameter = JSON.stringify(ine);
+        $http.post('app/dummy.json', parameter).
+        success(function(data, status, headers, config) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log(data);
+          }).
+          error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+          });
+  }
+
+});
